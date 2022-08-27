d607c8a42bdef0b2b90c23190a5350090f2ce192
Jack Guan <cnboyuguan@gmail.com>
2022-08-27 23:21:04 +0800

    将已完成的单线程设置为proxy_seq，开始写多线程

---
+++ labs/proxylab/proxy_seq.c
@@ -0,0 +1,170 @@
+/*
+ * @Author: Jack Guan cnboyuguan@gmail.com
+ * @Date: 2022-08-15 00:43:27
+ * @LastEditors: Jack Guan cnboyuguan@gmail.com
+ * @LastEditTime: 2022-08-27 23:10:54
+ * @FilePath: /guan/CSAPP/CSAPP-Labs/labs/proxylab/proxy.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// #include <stdio.h>
+#include "csapp.h"
+
+/* Recommended max cache and object sizes */
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+
+/* You won't lose style points for including this long line in your code */
+static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3";
+static const char *connection_hdr = "Connection: close";
+static const char *proxy_connection_hdr = "Proxy-Connection: close";
+
+
+void handleClient(int connectFd);
+void readHeaders(rio_t *rp);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+
+int main(int argc, char **argv)
+{
+    int listenFd, connectFd;
+    char clientHostName[MAXLINE], clientPort[MAXLINE]; 
+    // check form
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+    
+
+    listenFd = Open_listenfd(argv[1]);
+    
+    socklen_t clientLen;
+    struct sockaddr_storage clientAddr;
+    while (1)
+    {
+        clientLen = sizeof(clientAddr);
+        connectFd = Accept(listenFd, (SA *)& clientAddr, &clientLen);
+        Getnameinfo((SA *)&clientAddr, clientLen, clientHostName, MAXLINE, clientPort, MAXLINE, 0);
+        printf("\n########### new proxy###########\n##############\n");
+        printf("Accept connection from (%s,%s), content is :\n", clientHostName, clientPort);
+        handleClient(connectFd);
+        Close(connectFd);
+    }
+    
+    printf("%s", user_agent_hdr);
+    return 0;
+}
+
+void handleClient(int connectFd){
+    int i;
+    char buf[MAXLINE], contentProxyToServer[MAXLINE], request[MAXLINE], contentBackToClinet[MAXLINE];
+    strcpy(buf, "");
+    strcpy(contentProxyToServer, "");
+    strcpy(request, "");
+    strcpy(contentBackToClinet, "");
+    char method[32], uri[1024], httpVersion[64], targetHostNameAndPort[128], fileName[128];
+    strcpy(method, "");
+    strcpy(uri, "");
+    strcpy(httpVersion, "");
+    strcpy(targetHostNameAndPort, "");
+    strcpy(fileName, "");
+
+    rio_t clientRio, targetRio;
+    Rio_readinitb(&clientRio, connectFd);
+    if (!Rio_readlineb(&clientRio, request, MAXLINE))  // GET requests
+        return; // empty requests
+    printf("%s", request);
+    readHeaders(&clientRio);
+
+    sscanf(request, "%s %s %s", method, uri, httpVersion);
+    strcpy(httpVersion, "HTTP/1.0");
+    char *p = strstr(uri, "http://");
+    if (!p) {                     // if "http://" not in uri
+        p = uri;
+    } else{
+        p += 7;         // p skip "http://" to host name
+    }
+    i = 0;
+    while (*p != '/')
+    {
+        targetHostNameAndPort[i++] = *p;
+        p += 1;
+    }
+    targetHostNameAndPort[i] = '\0';
+    // finish the content from proxy to target server
+    strcpy(fileName, p);
+    sprintf(contentProxyToServer, "%s %s %s\r\n", method, fileName, httpVersion);
+    sprintf(contentProxyToServer, "%sHost: %s\r\n", contentProxyToServer,targetHostNameAndPort);
+    sprintf(contentProxyToServer, "%s%s\r\n", contentProxyToServer, user_agent_hdr);
+    sprintf(contentProxyToServer, "%s%s\r\n", contentProxyToServer, connection_hdr);
+    sprintf(contentProxyToServer, "%s%s\r\n\r\n", contentProxyToServer, proxy_connection_hdr);
+    char targetServerName[64], targetPort[16];
+    p = strstr(targetHostNameAndPort, ":");
+    if(!p){
+        // no port specifiction, then choose http port 80
+        strcpy(targetServerName, targetHostNameAndPort);
+        strcpy(targetPort, "80");
+    }
+    else{
+        strcpy(targetPort, p+1);
+        for ( i = 0; targetHostNameAndPort[i] != ':'; i++)
+        {
+            targetServerName[i] = targetHostNameAndPort[i];
+        }
+        targetServerName[i] = '\0';
+    }
+
+    printf("open a connection to target\ncontent is\n%s", contentProxyToServer);
+    // open a connect from proxy to target
+    int connectToTargetFd = Open_clientfd(targetServerName, targetPort);
+    if (connectToTargetFd < 0){  // connect to server 
+        clienterror(connectFd, method, "404", "connect failure", 
+        "the proxy server connect to target server failure");
+        return;
+    }
+    
+    Rio_writen(connectToTargetFd, contentProxyToServer, strlen(contentProxyToServer));
+    Rio_readinitb(&targetRio, connectToTargetFd);
+
+    while( Rio_readlineb(&targetRio, buf, MAXLINE) > 0 ){
+        strcat(contentBackToClinet, buf);
+    }
+    printf("\ncontent back to client is: \n%s", contentBackToClinet);
+    Rio_writen(connectFd, contentBackToClinet, strlen(contentBackToClinet));
+    Close(connectToTargetFd);
+    
+}
+
+void readHeaders(rio_t *rp){
+    // char buf[MAXLINE];
+    // Rio_readlineb(rp, buf, MAXLINE);
+    // printf("%s", buf);
+    // while (strcmp(buf, "\r\n"))
+    // {
+    //     Rio_readlineb(rp, buf, MAXLINE);
+    //     printf("%s", buf);
+    // }
+    return ;   
+}
+
+
+void clienterror(int fd, char *cause, char *errnum, 
+		 char *shortmsg, char *longmsg) 
+{
+    char buf[MAXLINE], body[MAXBUF];
+
+    /* Build the HTTP response body */
+    sprintf(body, "<html><title>Tiny Error</title>");
+    sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+    sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+    sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+    sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+
+    /* Print the HTTP response */
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+    Rio_writen(fd, buf, strlen(buf));
+    Rio_writen(fd, body, strlen(body));
+}
